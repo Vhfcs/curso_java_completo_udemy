fa8e86e5a5c2bd57c15b2bdc87f835a1d9465530
victor Hugo <hugof.victor@gmail.com>
2023-05-30 14:46:12 -0300

    Entendimentos sobre o capítulo de data e horas

---
+++ entendimento_data_hora_calculos/src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.Duration;
+
+public class Program {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2023-05-29");
+        LocalDateTime d05 = LocalDateTime.parse("2023-05-29T06:20:35");
+        Instant d06 = Instant.parse("2023-05-29T06:20:35Z");
+
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDateTime);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDateTime);
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("pastWeekInstant = " + pastWeekInstant);
+        System.out.println("nextWeekInstant = " + nextWeekInstant);
+
+        Duration t1 = Duration.between(pastWeekLocalDate.atTime(0, 0), d04.atTime(0,0));
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+        Duration t4 = Duration.between(d06, pastWeekInstant);
+
+        System.out.println("t1 dias = " + t1.toDays());
+        System.out.println("t2 dias = " + t2.toDays());
+        System.out.println("t3 dias = " + t3.toDays());
+        System.out.println("t4 dias = " + t4.toDays());
+
+    }
+}
--- entendimento_data_hora_texto/src/Program.java
+++ entendimento_data_hora_texto/src/Program.java
@@ -1,6 +1,7 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program {
@@ -9,6 +10,9 @@ public class Program {
         //https://download.java.net/java/early_access/jdk20/docs/api/java.base/java/time/format/DateTimeFormatter.html
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 
         LocalDate d04 = LocalDate.parse("2023-05-29");
         LocalDateTime d05 = LocalDateTime.parse("2023-05-29T06:20:35");
@@ -17,5 +21,11 @@ public class Program {
         System.out.println("d04 = " + d04.format(fmt1));
         System.out.println("d04 = " + fmt1.format(d04));
         System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+        System.out.println("d05 = " + d05.format(fmt2));
+        System.out.println("d05 = " + d05.format(fmt4));
+
+        System.out.println("d06 = " + fmt3.format(d06));
+        System.out.println("d06 = " + fmt5.format(d06));
     }
 }

ed988c11396bc6831831441a92363cbeac89f022
victor Hugo <hugof.victor@gmail.com>
2023-06-05 08:43:47 -0300

    Exercício sobre enumeração

--- exercicio_data_3/src/Main.java
+++ exercicio_data_3/src/Main.java
@@ -1,25 +1,27 @@
-package application;
-
 import javax.swing.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class Program {
-
-    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
+/**
+ * Descrição: calcula a diferença entre duas datas
+ * Desenvolvedor: Victor Hugo
+ */
+public class Main {
     public static void main(String[] args) throws ParseException {
 
+ //Criação do OptionPane
         String dataAtual = JOptionPane.showInputDialog(null, "Digite a data atual:");
         String dataNascimento = JOptionPane.showInputDialog(null, "Digite a data de nascimento");
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
         Date dataAtualFormatada = simpleDateFormat.parse(dataAtual);
         Date dataNascimentoFormatada = simpleDateFormat.parse(dataNascimento);
 
-        c
-
+        String strDatatual = simpleDateFormat.format(dataAtualFormatada);
+        String strDataNascimento = simpleDateFormat.format(dataNascimentoFormatada);
         String anoDataAtual = strDatatual.substring(6,10);
         String anoDataNascimento = strDataNascimento.substring(6,10);
 
@@ -32,10 +34,5 @@ public class Program {
 
 
 
-
-
-
-
-
     }
-}
+        }
